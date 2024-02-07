6a5a7233e0bfdc17cc45589f28d4103801198430
KurtyMittens <134671520+KurtyMittens@users.noreply.github.com>
2024-02-07 22:30:43 +0800

    Add files via upload

---
+++ Arduino/LoginSystem/LoginSystem.ino
@@ -0,0 +1,47 @@
+int leds[2] = {2, 3};
+String username = "";
+String password = "";
+int state = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for (int i =0 ; i < 2; i++){
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Enter your Username: ");
+  while (Serial.available() == 0){
+  }
+    username = Serial.readString();
+    username.trim();
+
+   Serial.println("Enter your Password: ");
+   while (Serial.available() == 0){
+  }
+    password = Serial.readString();
+    password.trim();
+
+  if((username.length()>0) && (password.length()>0)){
+    if (username.equals("admin")){
+      if (password.equals("admin")){
+        Serial.println("GOODSHIT!");
+        digitalWrite(leds[0], HIGH);
+        digitalWrite(leds[1], LOW);
+      }
+      else {
+        Serial.println("password WRONG");
+        digitalWrite(leds[0], LOW);
+        digitalWrite(leds[1], HIGH);
+      }
+    }
+    else {
+      Serial.println("username WRONG");
+      digitalWrite(leds[0], LOW);
+      digitalWrite(leds[1], HIGH);
+      }
+    }
+}
